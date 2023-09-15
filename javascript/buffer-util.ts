22d8c9f36e9a65cd5bd453541cf3f2ae12009414
Xiongfs <121023254@qq.com>
2023-09-15 16:59:57 +0800

    feat:fix

--- javascript/buffer-util.ts
+++ javascript/buffer-util.ts
@@ -1,23 +1,23 @@
 // THX: https://mp.weixin.qq.com/s/2M_8K47I_fON9ba8nZODNw
 
-// 文本转二进制数组
-export function text2BinaryList(text) {
-    // 文本转buffer
+// 文本转字节数组
+export function text2ByteArray(text) {
+    // 文本转字节流
     const encoder = new TextEncoder()
-    const buffer = encoder.encode(text)
+    const view = encoder.encode(text)
     // buffer转二进制
-    return Array.from(buffer)
+    return Array.from(view)
 }
 
-// 二进制数组转字符串
-export function binaryList2Text(binaryList, encoding = 'utf-8') {
-    // 二进制转buffer
-    const buffer = new ArrayBuffer(binaryList.length)
+// 字节数组转文本
+export function byteArray2Text(byteList, encoding = 'utf-8') {
+    // 根据字节数组生成buffer
+    const buffer = new ArrayBuffer(byteList.length)
     const view = new Uint8Array(buffer);
-    for (var i = 0; i < binaryList.length; ++i) {
-        view[i] = binaryList[i]
+    for (var i = 0; i < byteList.length; ++i) {
+        view[i] = byteList[i]
     }
-    // buffer转字符串
+    // 字节流转文本
     const decoder = new TextDecoder(encoding)
     return decoder.decode(buffer)
 }
